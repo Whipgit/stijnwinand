d6193100b6a331b801a9599372ca82a1efe636ba
Whiplash88 <whiplash174@gmail.com>
2016-06-17 14:17:26 -0400

    block out boot script

--- server/server.js
+++ server/server.js
@@ -5,23 +5,23 @@ var path = require('path');
 var app = module.exports = loopback();
 
 app.start = function() {
-  
+
   // start the web server
   return app.listen(function() {
     var baseUrl = app.get('url').replace(/\/$/, '');
     var explorerPath;
-    
+
     app.emit('started');
-    
+
     console.log('Web server listening at: %s', baseUrl);
-    
-    if (process.env.NODE_ENV !== 'production' && 
+
+    if (process.env.NODE_ENV !== 'production' &&
       app.get('loopback-component-explorer')) {
-        
+
       explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
-    
+
     mountAngular();
   });
 };
@@ -35,23 +35,23 @@ boot(app, __dirname, function(err) {
   if (require.main === module)
     app.start();
 });
-  
+
 
 function mountAngular() {
   const staticPath = path.resolve(__dirname, '../client/dist/');
-  
+
   app.use(loopback.static(staticPath));
 
   // any other routes:
   app.use('/*', function(req, res, next) {
-    
+
     if (req.originalUrl.includes('api')) {
       return next();
-    } 
+    }
     if (req.originalUrl.includes('js.map')) {
       return next();
     }
-    
+
     res.sendFile(staticPath + '/index.html');
   });
 }
